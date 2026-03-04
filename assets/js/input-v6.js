2c096e44e8ec22a5e9d4b7be6160c611be41ac2a
Justin Kruit <justinkruitnl@gmail.com>
2026-03-04 17:30:55 +0100

    Implement more html escaping

--- assets/js/input-v6.js
+++ assets/js/input-v6.js
@@ -6,10 +6,11 @@
 		} else {
 			try {
 				var iconJson = $.parseJSON( value );
+				console.log(iconJson);
 
 				if ( 'object' === typeof iconJson ) {
-					$( '.acf-field-setting-fa_live_preview .acf-input', parent ).html( '<i class="fa-' + iconJson.family + ' fa-' + iconJson.style + ' fa-' + iconJson.id + ' fa-fw" aria-hidden="true"></i>' );
-					$( '.icon_preview', parent ).html( '<i class="fa-' + iconJson.family + ' fa-' + iconJson.style + ' fa-' + iconJson.id + ' fa-fw" aria-hidden="true"></i>' );
+					$( '.acf-field-setting-fa_live_preview .acf-input', parent ).html( acf.escHtml('<i class="fa-' + iconJson.family + ' fa-' + iconJson.style + ' fa-' + iconJson.id + ' fa-fw" aria-hidden="true"></i>') );
+					$( '.icon_preview', parent ).html( acf.escHtml('<i class="fa-' + iconJson.family + ' fa-' + iconJson.style + ' fa-' + iconJson.id + ' fa-fw" aria-hidden="true"></i>') );
 					$( '.icon_preview', parent ).removeClass('v5-compat-alert show-alert');
 				}
 			} catch( err ) {
