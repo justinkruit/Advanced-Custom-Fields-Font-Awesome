b1825a2bbc728650f5eac7e96b89f65def2ea8ab
Justin Kruit <justinkruitnl@gmail.com>
2026-05-12 16:47:07 +0200

    Be able to clear the default icon

--- assets/js/fa-icon-chooser.esm.js
+++ assets/js/fa-icon-chooser.esm.js
@@ -204,7 +204,12 @@ import 'https://cdn.jsdelivr.net/npm/@fortawesome/fa-icon-chooser@0.10.2/dist/fa
 
     let inputWrapper = defaultValueWrapper.find('.acf-input');
     inputWrapper.prepend(
-      '<div class="icon_preview"></div><button type="button" class="fa-icon-chooser-open button">Choose icon</button>',
+      `<div class="icon_preview"></div><button type="button" class="fa-icon-chooser-open button">Choose icon</button>
+      			<button type="button" class="fa-icon-clear" title="Clear icon">
+					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640"><!--!Font Awesome Free v7.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2026 Fonticons, Inc.-->
+						<path d="M183.1 137.4C170.6 124.9 150.3 124.9 137.8 137.4C125.3 149.9 125.3 170.2 137.8 182.7L275.2 320L137.9 457.4C125.4 469.9 125.4 490.2 137.9 502.7C150.4 515.2 170.7 515.2 183.2 502.7L320.5 365.3L457.9 502.6C470.4 515.1 490.7 515.1 503.2 502.6C515.7 490.1 515.7 469.8 503.2 457.3L365.8 320L503.1 182.6C515.6 170.1 515.6 149.8 503.1 137.3C490.6 124.8 470.3 124.8 457.8 137.3L320.5 274.7L183.1 137.4z" />
+					</svg>
+				</button>`,
     );
 
     // Initialize preview based on existing value
@@ -227,6 +232,14 @@ import 'https://cdn.jsdelivr.net/npm/@fortawesome/fa-icon-chooser@0.10.2/dist/fa
       inputEl = wrapper.find('input');
 
       openIconChooser();
+    });
+
+    $el.find('.fa-icon-clear').on('click', function () {
+      let wrapper = $(this).closest('.acf-input');
+      let input = wrapper.find('input');
+      let preview = wrapper.find('.icon_preview');
+      input.val('').trigger('change');
+      preview.empty();
     });
   }
 
